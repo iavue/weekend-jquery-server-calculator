1d4f3ab489d112cc6143efe73b92cc2d532ac2d3
Ia Vue <iavue00@gmail.com>
2023-03-04 22:47:59 -0600

    completed logic for all operators and was able to append answers and history to the dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,19 @@
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('jQUery is loaded!');
+    console.log('jQuery is loaded!');
 
     // Listener for addition button
     $('#additionBtn').on('click', additionOperator);
+
+    // Listener for addition button
+    $('#subtractionBtn').on('click', subtractionOperator);
+
+    // Listener for addition button
+    $('#multiplicationBtn').on('click', multiplicationOperator);
+
+    // Listener for addition button
+    $('#divisionBtn').on('click', divisionOperator);
 
     // Listener for equal button
     $('#equalBtn').on('click', sendEquation);
@@ -15,18 +24,38 @@ function onReady() {
 
 let operatorSign = '';
 
-// will probably have to add more functions for the rest of the operators
+// Functions for all operators. This is to detect what operator was clicked by the user.
+// The global variable "operatorSign" will be set based on the operator detected.
 function additionOperator() {
     console.log('Inside additionOperator()');
     operatorSign = '+';
     console.log('Addition operator:', operatorSign);
 }
 
+function subtractionOperator() {
+    console.log('Inside subtractionOperator()');
+    operatorSign = '-';
+    console.log('Subtraction operator:', operatorSign);
+}
+
+function multiplicationOperator() {
+    console.log('Inside multiplicationOperator()');
+    operatorSign = '*';
+    console.log('Multiplication operator:', operatorSign);
+}
+
+function divisionOperator() {
+    console.log('Inside divisionOperator()');
+    operatorSign = '/';
+    console.log('Division operator:', operatorSign);
+}
+
 // POST
-// Send equation object (num1, operatorsign, num2) to the server
+// After detecting operatorSign, send data (which is an object of num1, operatorsign, num2) to the server
 function sendEquation() {
     console.log('Inside sendEquation()');
 
+    // This is the object sending to the server. We send it below in the ajax data.
     let objectToSend = {
         num1: $('#num1Input').val(),
         operatorSign,
@@ -36,7 +65,7 @@ function sendEquation() {
     $.ajax({
         method: 'POST',
         url: '/putInEquationInputs',
-        data: objectToSend
+        data: objectToSend // The object is sent to line 44 of server.js
     }).then((response) => {
         console.log('Post finished.');
         getEquations();
@@ -45,16 +74,15 @@ function sendEquation() {
     })
 }
 
-
+// GET
 // This is where we get the equations from the server side
-// And then....
 function getEquations() {
     $.ajax({
         method: 'GET',
-        url: '/equationInputs'
-    }).then((response) => {
+        url: '/equationInputs' // go to line 37 of server.js
+    }).then((response) => { // Response now has data from line 39 of server.js
         console.log('equations data:', response);
-        // To do: append equations to DOM
+        // To do: append response (equations) to DOM
         renderToDom(response);
     }).catch((response) => {
         alert('Request failed. Unable to get equations data.')
@@ -62,16 +90,20 @@ function getEquations() {
 }
 
 
-function renderToDom(incomingArray) {
+function renderToDom(incomingArray) { // We called the render on line 86 with response in the parameters
     console.log('Inside renderToDom()');
 
-    // Append answer to the #resultOutput
-    // $('#resultOutput').empty('');
+    /* Empty the #historyOutput only once before the loop so we can append
+    the history of ALL the equations in the incomingArray (response) */
     $('#historyOutput').empty('');
 
+    // Loop through incomingArray (response) to append the answer and keep a history of each equation
     for (let equation of incomingArray) {
+        // Empty the #resultOutput during each loop
         $('#resultOutput').empty('');
+        // Then append the answer to the dom
         $('#resultOutput').append(`${equation.answer}`);
+        // Append a history of the equations inside <li></li>
         $('#historyOutput').append(`
             <li>
                 ${equation.num1}
@@ -84,6 +116,7 @@ function renderToDom(incomingArray) {
     }
 }
 
+// Function to clear the input fields after click the 'C' button
 function clearInput() {
     $('#num1Input').val('');
     $('#num2Input').val('');
--- server/server.js
+++ server/server.js
@@ -32,38 +32,51 @@ app.listen(port, () => {
 let equations = [];
 
 // GET
-// This is where we will get our data (equations) after receiving it from client side
-// Then respond (send) back the equations object to the client
+// This is where we will get our data (which is the equations[]) after receiving it from client side in app.post
 app.get('/equationInputs', (req, res) => {
   console.log('in /equationInputs GET');
-  // respond/send data which is the object storing our data
-  res.send(equations);
+  
+  res.send(equations); // Here we respond/send data which is the object(s) in equations[] to line 84 of client.js
 });
 
 // POST
 app.post('/putInEquationInputs', (req,res) => {
   console.log('in POST /putInEquationInputs', req.body);
-  // data from client side will be put on req.body. So objectToSend teleported to req.body
+  // data from client side will be put on req.body. So "objectToSend" teleported to req.body!
 
-  let operator = req.body.operatorSign;
+  // Variables are declared here so we can use them to start calculating!
+  let operator = req.body.operatorSign; // this is the operatorSign from "objectToSend"
   let firstNum = req.body.num1;
   let secondNum = req.body.num2;
-  let answer = 0;
+  let answer = 0; /* Since we don't have the "answer" property in req.body (objectToSend), we need to 
+  declare a variable for 'answer' so we can capture the answer and append it to the DOM */
 
-  // calculate equation from req.body
+  // Create If statements to calculate equation(s) from req.body (objectToSend) for each operator.
   if (operator === '+') {
-    answer = Number(firstNum) + Number(secondNum);
+    answer = Number(firstNum) + Number(secondNum); // The variables were strings so we need to convert them to numbers to calculate.
+    console.log('Answer:', answer);
+  }
+  else if (operator === '-') {
+    answer = Number(firstNum) - Number(secondNum);
+    console.log('Answer:', answer);
+  }
+  else if (operator === '*') {
+    answer = Number(firstNum) * Number(secondNum);
+    console.log('Answer:', answer);
+  }
+  else {
+    answer = Number(firstNum) / Number(secondNum);
     console.log('Answer:', answer);
   }
 
-  // add answer to equation[] object
+  // Create a new property for "answer" inside the object (req.body) and set it to the answer (which comes from the calculation in the If statement)
   req.body.answer = answer;
-  console.log('req.body after adding answer property:', req.body);
+  console.log('req.body after adding answer property:', req.body); // We need to see if the "answer" property was properly added
 
-  // store the equation from objectToSend
+  // Store the equation from objectToSend which should have the new answer property now.
   equations.push(req.body);
 
   console.log('Equations:', equations);
 
-  res.sendStatus(201);
+  res.sendStatus(201); // Respond to client side at line 69 with 201 status
 });
